4f4f12c2f9b2df1a5f3f938ec35d63d2daeb70a5
AdrianABV99 <57294115+AdrianABV99@users.noreply.github.com>
2020-05-16 18:11:47 +0300

    St 6 (#26)
    * ST6 COMMIT
    
    * ST6 COMMIT

--- src/main/java/sef/proj/FileSystemService.java
+++ src/main/java/sef/proj/FileSystemService.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileSystemService {
-    private static final String APPLICATION_FOLDER = ".Schedule";
+ /*name of the created folder */  private static final String APPLICATION_FOLDER = ".Schedule";
     private static final String USER_FOLDER = System.getProperty("user.home");
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
