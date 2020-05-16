3053a4e4344428d7edbcd9a56909a5897855f390
Adrian Achim <adrian99achim@gmail.com>
2020-05-16 18:11:39 +0300

    ST26 COMMIT

--- src/main/java/sef/proj/FileSystemService.java
+++ src/main/java/sef/proj/FileSystemService.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class FileSystemService {
  /*name of the created folder */  private static final String APPLICATION_FOLDER = ".Schedule";
-    private static final String USER_FOLDER = System.getProperty("user.home");
+ /* whew u find the folder */   private static final String USER_FOLDER = System.getProperty("user.home");
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
     public static Path getPathToFile(String... path) {
