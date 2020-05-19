d38464696bf79ee5d8a79a2bfb34d2b05bb531d4
Adrian Achim <adrian99achim@gmail.com>
2020-05-19 10:15:46 +0300

    Second Test Added

--- src/test/java/sef/proj/services/UserServiceTest.java
+++ src/test/java/sef/proj/services/UserServiceTest.java
@@ -9,11 +9,20 @@ import sef.proj.services.UserService;
 class UserServiceTest {
 
     @Test
-    void addUserTest() {
+    void nameUserTest() {
         UserService test = new UserService();
 
         for (User name : test.getUsers()){
             Assert.assertEquals(name.getUserName().matches("^[\\\\p{L} .'-]+$"),name.getUserName());
+        }
+    }
+
+    @Test
+    void tagTest() {
+        UserService test = new UserService();
+
+        for (User tag : test.getUsers()) {
+            Assert.assertEquals(tag.getUserName().matches("(([01]?[0-9]):([0-5][0-9]) ([AaPp][Mm]))"),tag.getTag());
         }
     }
 }
