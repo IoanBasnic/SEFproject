16661ffb804aefbfa4f837415309aed1afe0e084
Basnic Ioan <basnic.ioan@gmail.com>
2020-05-19 22:33:32 +0300

    Bugfix on view schedule

--- src/main/java/sef/proj/controllers/ViewController.java
+++ src/main/java/sef/proj/controllers/ViewController.java
@@ -3,9 +3,11 @@ package sef.proj.controllers;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import sef.proj.services.TaskService;
 import sef.proj.services.UserService;
 
 import java.io.IOException;
@@ -16,15 +18,15 @@ public class ViewController implements Initializable {
 
 
     @FXML
-    private TextField Description;
+    private Label description;
 
     @FXML
-    private JFXButton Exit;
+    private JFXButton btnExit;
     @FXML
-    private void ChangePass(MouseEvent event) {
-        if(event.getSource() == Exit) {
+    private void Exit(MouseEvent event) {
+        if(event.getSource() == btnExit) {
 
-            Stage stage = (Stage) Exit.getScene().getWindow();
+            Stage stage = (Stage) btnExit.getScene().getWindow();
             stage.close();
         }
     }
@@ -32,9 +34,11 @@ public class ViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            UserService.loadUsersFromFile();
+            TaskService.loadUsersFromFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        description.setText(TaskService.getDescription(1));
     }
 }
